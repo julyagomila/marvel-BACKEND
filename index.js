a56b26a963b837afd086a3e2c0436d8caab397ec
julyagomila <gomila.julia19@gmail.com>
2022-05-11 16:15:42 +0200

    upload1

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const formidableMiddleware = require("express-formidable");
+const cors = require("cors");
+app.use(formidableMiddleware());
+app.use(cors());
+const axios = require("axios");
+require("dotenv").config();
+
+const API_KEY = process.env.API_KEY;
+
+app.get("/getAllCharacters", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${API_KEY}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server has started");
+});
